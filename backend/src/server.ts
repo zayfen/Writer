aaaaf00c335f70a07ce26a5d01265759b60ef0f0
zayfen <zhangyunfeng0101@gmail.com>
2019-09-09 20:49:18 +0800

    chore: add backend project

---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import * as Koa from 'koa'
+import * as Router from 'koa-router'
+
+const app = new Koa()
+const router = new Router()
+
+
+router.get('/*', async (ctx: Koa.Context) => {
+  ctx.body = "Hello World!"
+})
+
+app.use(router.routes())
+
+app.listen(3000);
+
+console.log("Server Running On Port 3000")
+
+
