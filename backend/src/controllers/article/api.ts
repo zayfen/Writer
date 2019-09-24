a1661388f78189d24fb695d0f0107516b7654b58
zayfen <zhangyunfeng0101@gmail.com>
2019-09-24 17:25:31 +0800

    chore: 完成后台的搭建；增加鉴权逻辑

--- backend/src/controllers/article/api.ts
+++ backend/src/controllers/article/api.ts
@@ -4,17 +4,21 @@
 import * as Koa from 'koa'
 import { GET, POST, MIDDLEWARE } from '../../core/decorators'
 import { BaseRouter, MiddleWare } from '../../core/types'
+import ArticleService from '../../services/article_service'
 
 class Index implements BaseRouter {
   prefix: string = '/api/article'
+  articleService = new ArticleService()
 
   public classMiddlewares(): Array<MiddleWare | string> {
     return ['auth']
   }
 
   @GET('/list')
-  public getArticleList (ctx: Koa.Context) {
-    
+  public async getArticleList (ctx: Koa.Context) {
+    let user: string = ctx.session.user
+    let articles = await this.articleService.getArticlesByUser(user)
+    ctx.body = articles
   }
 }
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,18 +5,42 @@ import { boot } from './core/boot'
 
 const app = new Koa()
 const router = new Router()
+import * as session from 'koa-session'
+
+app.keys = ['abcK2?xZ', '12D@KiS::)d', 'kSJDfkjwe(23r}{']
+
+app.use(async (ctx, next) => {
+  console.log("request: ", ctx.originalUrl)
+  // console.log("router.stack", router.stack)
+  try {
+    await next()  
+  } catch (error) {
+    ctx.app.emit('error', error, ctx)
+  }
+})
+
+const CONFIG = {
+  key: 'sid', 
+  maxAge: 86400000,
+  autoCommit: true, /** (boolean) automatically commit headers (default true) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+}
+// install session
+app.use(session(CONFIG, app))
 
 // initialized global
-
 boot(router)
 
 
-// router.get('/*', async (ctx: Koa.Context) => {
-//   ctx.body = "Hello World!"
-// })
-
-
 app.use(router.routes())
+
+app.on('error', (err, ctx) => {
+  console.error("Error: ", err)
+})
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT);
